5572b3da075fe5b660e1cd74ccab3f12469b9176
Daniela Castro <115217686+arbyun@users.noreply.github.com>
2026-05-23 02:54:23 +0100

    fix: correct css filename case and remove duplicate deploy step (#1)

--- Portfolio/obj/Release/net8.0/Portfolio.AssemblyInfo.cs
+++ Portfolio/obj/Release/net8.0/Portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59e0061daee3c27605216a850a9b701233b7def5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4267cf2f03307ac15c49853d4ae8fe4a4c12f215")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
