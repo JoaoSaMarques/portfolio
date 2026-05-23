4267cf2f03307ac15c49853d4ae8fe4a4c12f215
Daniela Castro <115217686+arbyun@users.noreply.github.com>
2026-05-23 02:41:16 +0100

    fix corrupted cache

--- Portfolio/obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
+++ Portfolio/obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42db6f14e646cea702d2cb5fcf3a87bc8217d78a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59e0061daee3c27605216a850a9b701233b7def5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Portfolio/obj/Release/net8.0/Portfolio.AssemblyInfo.cs
+++ Portfolio/obj/Release/net8.0/Portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+764e1076f67e33734b5d0d00161ba73e8165d3b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59e0061daee3c27605216a850a9b701233b7def5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
